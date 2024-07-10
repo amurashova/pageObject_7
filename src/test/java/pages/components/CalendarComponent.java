b2c0488646c4b1cb6d184523c974224e4bd89b1b
anastasiamurasova <nomofamilinomo@gmail.com>
2024-07-10 13:26:03 +0300

    change calendat component

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -6,6 +6,7 @@ public class CalendarComponent {
     public void setDate(String day, String month, String year) {
         $(".react-datepicker__year-select").selectOption(year);
         $(".react-datepicker__month-select").selectOption(month);
-        $(String.format(".react-datepicker__day--0%s:not(.react-datepicker__day--outside-month)", day)).click();
+        String dayClass = ".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)";
+        $(dayClass).click();
     }
 }
